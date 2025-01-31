1d1ee7bc2e6fa3a7cb07c6a96281a42f776a4d69
Kirill Somov <gornomirov661@gmail.com>
2025-01-31 14:26:00 +0300

    basic navigation to single image

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Сомов Кирилл on 31.01.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        // code
+    }
+}

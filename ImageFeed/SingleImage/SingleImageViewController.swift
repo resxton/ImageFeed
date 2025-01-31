fbbe1762728aac0ec01512af1a598ebde39a2485
Kirill Somov <gornomirov661@gmail.com>
2025-02-01 00:12:44 +0300

    fullscreen scrollview

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     // MARK: - IB Outlets
     @IBOutlet private var imageView: UIImageView!
+    
     @IBOutlet private var backButton: UIButton!
+    
+    @IBOutlet private var scrollView: UIScrollView!
     
     // MARK: - Public Properties
     var image: UIImage? {
@@ -24,11 +27,25 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupScrollView()
         imageView.image = image
     }
     
     // MARK: - IB Actions
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Private Methods
+    func setupScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.contentInsetAdjustmentBehavior = .never
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: -20),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: -20),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 }

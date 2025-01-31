fef7e960fc0093a624a708709963fd95daa48745
Kirill Somov <gornomirov661@gmail.com>
2025-02-01 01:48:12 +0300

    finished scroll view

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -18,8 +18,11 @@ final class SingleImageViewController: UIViewController {
     // MARK: - Public Properties
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
+
             imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
 
@@ -28,7 +31,10 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         
         setupScrollView()
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
     // MARK: - IB Actions
@@ -36,16 +42,72 @@ final class SingleImageViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func didTapShareButton(_ sender: Any) {
+        let text = "Смотри, какую красоту нашел!"
+        guard let sharingImage = image else { return }
+        
+        let activityViewController = UIActivityViewController(activityItems: [text, sharingImage], applicationActivities: nil)
+        
+        present(activityViewController, animated: true)
+    }
+    
+    @IBAction func didTapFavoritesButton(_ sender: Any) {
+        print("tapped favorites button")
+    }
+    
     // MARK: - Private Methods
-    func setupScrollView() {
+    private func setupScrollView() {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.contentInsetAdjustmentBehavior = .never
         
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: -20),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: -20),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        
+        imageView.frame.size = CGSize(width: imageSize.width * scale, height: imageSize.height * scale)
+        imageView.center = CGPoint(x: scrollView.bounds.midX, y: scrollView.bounds.midY)
+    }
+
+    
+    private func centerImageInScrollView(image: UIImage) {
+        let boundsSize = scrollView.bounds.size
+        let contentSize = scrollView.contentSize
+
+        let dx = max(0, floor((boundsSize.width - contentSize.width) / 2))
+        let dy = max(0, floor((boundsSize.height - contentSize.height) / 2))
+
+        scrollView.contentInset = UIEdgeInsets(top: dy, left: dx, bottom: dy, right: dx)
+        scrollView.layoutIfNeeded()
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        guard let image else { return }
+        centerImageInScrollView(image: image)
     }
 }

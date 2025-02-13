da393271abb52e23e398cb14e41fb8f419119575
Kirill Somov <gornomirov661@gmail.com>
2025-02-13 17:41:54 +0300

    before webview

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Сомов Кирилл on 13.02.2025.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        print("Auth view did load")
+    }
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Сомов Кирилл on 13.02.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "i0z8wFdFRDUo6-MtlF4nahpYEIHVM7uoPWk2WiJ-XEA"
+    static let secretKey = "TtuFvvyu1F0nlRenuHfUt26CPrjS7H3IeV_mEiZeWfw"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
